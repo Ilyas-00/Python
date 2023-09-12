01817e36b5493f11c2645a6c13c991769651cc9b
Ilyas AKIOUI <129302570+Ilyas-00@users.noreply.github.com>
2023-09-12 23:35:17 +0200

    Update Chalenge1.py

--- Chalenge1.py
+++ Chalenge1.py
@@ -3,7 +3,6 @@
 # You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
 
 # Return the array ans. Answers within 10-5 of the actual answer will be accepted.
-
 # Note that:
 
 # Kelvin = Celsius + 273.15
